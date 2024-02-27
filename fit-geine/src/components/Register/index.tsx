af040814e2441b5c2f820dc77f1e28b723715b13
mostafasayed2022 <116205995+mostafasayed2022@users.noreply.github.com>
2024-02-27 15:01:27 +0200

    bb

--- fit-geine/src/components/Register/index.tsx
+++ fit-geine/src/components/Register/index.tsx
@@ -1,110 +1,126 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { FaGoogle, FaFacebook, FaLinkedin } from 'react-icons/fa';
 import Navbar from "../Navbar";
 import Footer from "../Footer";
-import './register.css';
-import { FaGoogle, FaFacebook, FaLinkedin } from 'react-icons/fa';
 import services from "../../images/services.jpg";
 import contactBG from "../../images/contactBg.jpg";
-function Register() {
+import './register.css';
+import axios from 'axios';
 
-  const [password, setPassword] = useState('');
-  const [showPassword, setSowPassword] = useState(false);
+function Register() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    try{
+     const response =await axios.post(`https://localhost:8000/api/register/`,formData);
+     console.log(response.data);  
+  }
+  catch(error)
+  {
+    console.error(`Error registration :`,error);
+  }
+}
 
   const togglePasswordVisibility = () => {
-    setSowPassword(!showPassword);
+    setShowPassword(!showPassword);
   }
 
   return (
-    <body>
-      <body>
-        <Navbar />
-        <div className='logiiin' style={{ backgroundImage: `url(${services})` }}>
-          <h1> Register Page</h1>
-        </div>
-        <div className="containerrrrr" style={{ backgroundImage: `url(${contactBG})` }}>
-          <div className="login-register-container">
-            <div className='form-cont'>
-              <form  >
-                <div className="form-field-wrapper">
-                  <input
-                    required
-                    type="text"
-                    name="text"
-                    placeholder="Full name"
-                  />
-
-
-                  {/* {errors.email && <p style={{ color: 'red', fontSize: '13px' }}>{errors.email}</p>} */}
-                </div>
-
-
-
-                <div className="form-field-wrapper">
-                  <input
-                    required
-                    type="email"
-                    name="email"
-                    placeholder=" email"
-                  />
-
-
-                  {/* {errors.email && <p style={{ color: 'red', fontSize: '13px' }}>{errors.email}</p>} */}
-                </div>
-                <div className="form-field-wrapper">
-                  <input
-                    type={showPassword ? 'text' : 'password'}
-                    name="password"
-                    placeholder="Enter password..."
-                    autoComplete="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                  {/* {errors.password && <p style={{ color: 'red', fontSize: '13px' }}>{errors.password}</p>} */}
-                </div>
-                <div className="form-field-wrapper">
-                  <input
-                    type={showPassword ? 'text' : 'password'}
-                    name="password"
-                    placeholder="Confirm password..."
-                    autoComplete="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                  {/* {errors.password && <p style={{ color: 'red', fontSize: '13px' }}>{errors.password}</p>} */}
-                </div>
-                <div className="form-field-wrapper">
-                  <input
-                    type="submit"
-                    value="Login"
-                    className="btnnn"
-                  />
-                </div>
-                <div className='forget_password '>
-                  <div className='checkbox pass '>
-                    <label>
-                      <input
-                        type="checkbox"
-                        checked={showPassword}
-                        onChange={(togglePasswordVisibility)}
-                      />
-                      <span>Show password</span>
-                    </label>
-                  </div>
-                </div>
-              </form>
-              <p className='social'>sign in with  <span>social</span> </p>
-              <div className='social-media'>
-                <button className='btn-social' type="submit"><FaGoogle className='FaGoogle' />  </button>
-                <button className='btn-social' type="submit"><FaFacebook className='FaFacebook' />  </button>
-                <button className='btn-social' type="submit"><FaLinkedin className='FaLinkedin' />  </button>
+    <>
+      <Navbar />
+      <div className='logiiin' style={{ backgroundImage: `url(${services})` }}>
+        <h1> Register Page</h1>
+      </div>
+      <div className="containerrrrr" style={{ backgroundImage: `url(${contactBG})` }}>
+        <div className="login-register-container">
+          <div className='form-cont'>
+            <form onSubmit={handleSubmit}>
+              <div className="form-field-wrapper">
+                <input
+                  required
+                  type="text"
+                  name="fullName"
+                  // value={formData.fullName}
+                  onChange={handleInputChange}
+                  placeholder="Full name"
+                />
               </div>
+
+              <div className="form-field-wrapper">
+                <input
+                  required
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  placeholder="Email"
+                />
+              </div>
+
+              <div className="form-field-wrapper">
+                <input
+                  type={showPassword ? 'text' : 'password'}
+                  name="password"
+                  placeholder="Enter password..."
+                  autoComplete="Password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                />
+              </div>
+
+              <div className="form-field-wrapper">
+                <input
+                  type={showPassword ? 'text' : 'password'}
+                  name="confirmPassword"
+                  placeholder="Confirm password..."
+                  autoComplete="Password"
+                  // value={formData.confirmPassword}
+                  onChange={handleInputChange}
+                />
+              </div>
+
+              <div className="form-field-wrapper">
+                <input
+                  type="submit"
+                  value="Register"
+                  className="btnnn"
+                  onChange={handleSubmit}
+                />
+              </div>
+
+              <div className='forget_password '>
+                <div className='checkbox pass '>
+                  <label>
+                    <input
+                      type="checkbox"
+                      checked={showPassword}
+                      onChange={togglePasswordVisibility}
+                    />
+                    <span>Show password</span>
+                  </label>
+                </div>
+              </div>
+            </form>
+            <p className='social'>Sign in with  <span>social</span> </p>
+            <div className='social-media'>
+              <button className='btn-social' type="submit"><FaGoogle className='FaGoogle' />  </button>
+              <button className='btn-social' type="submit"><FaFacebook className='FaFacebook' />  </button>
+              <button className='btn-social' type="submit"><FaLinkedin className='FaLinkedin' />  </button>
             </div>
           </div>
         </div>
-      </body>
+      </div>
       <Footer />
-    </body>
+    </>
   );
 }
 
