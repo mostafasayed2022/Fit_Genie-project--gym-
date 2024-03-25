e05b1b2dd76b0d3fdb91f5d14a0450ceadb0a727
mostafasayed2022 <116205995+mostafasayed2022@users.noreply.github.com>
2024-03-26 00:46:57 +0200

    548

--- fit-geine/src/components/Footer/index.tsx
+++ fit-geine/src/components/Footer/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './footer.css';
-import logo from "../../images/logo.ico";
+// import logo from "../../images/logo.ico";
+import logo from "../../images/loggo.jpg";
+
 import  { Link } from 'react-router-dom';
 
 import contactBG from "../../images/contactBg.jpg";
