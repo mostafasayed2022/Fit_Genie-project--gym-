97b0e8eac3dbc9f4c7a2486a30cc705eb4a4d06e
mostafasayed2022 <116205995+mostafasayed2022@users.noreply.github.com>
2024-05-13 23:51:32 +0300

    sfdg

--- fit-geine/src/components/NutrationPlan/Nutration.tsx
+++ fit-geine/src/components/NutrationPlan/Nutration.tsx
@@ -1,58 +1,70 @@
 import React, { useState } from "react";
-import axios from "axios";
 import Navbar from "../Navbar";
 
 interface WorkoutPlan {
     Title: string;
-    Calories: number;
-    Carbs: number;
+    Calories: string;
 }
 
-const NutrationPlan = () => {
+const Nutration = () => {
     const [workoutPlans, setWorkoutPlans] = useState<WorkoutPlan[]>([]);
-    const [isFetching, setIsFetching] = useState<boolean>(false);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
     const [email, setEmail] = useState<string>('');
 
-
-    const fetchWorkoutPlans = async () => {
-        setIsFetching(true);
+    const fetchWorkoutPlans = async (email: string) => {
+        setIsLoading(true);
         try {
-            const response = await axios.get<WorkoutPlan[]>('https://127.0.0.1:8000/api/mealplan/');
-            setWorkoutPlans(response.data);
+            const response = await fetch('https://127.0.0.1:8000/api/mealplan/', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email: email })
+            });
+            console.log(response)
+            const data = await response.json();
+            setWorkoutPlans(data);
         } catch (error) {
             console.error('Error fetching data:', error);
         }
-        setIsFetching(false);
+        setIsLoading(false);
     };
 
     const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setEmail(e.target.value);
     };
 
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        fetchWorkoutPlans(email);
+    };
 
     return (
         <>
             <Navbar loggedIn={undefined} />
             <div>
-                <h1>Nutration Plans</h1>
-
-                <input
-                    type="email"
-                    value={email}
-                    onChange={handleEmailChange}
-                    placeholder="Enter your email"
-                />
-                <button onClick={fetchWorkoutPlans} disabled={isFetching}>
-                    {isFetching ? 'Fetching...' : 'Fetch Nutration Plans'}
-                </button>
+                <h1>Workout Plans</h1>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={handleEmailChange}
+                        placeholder="Enter your email"
+                        required
+                    />
+                    <button onClick={() => fetchWorkoutPlans} >
+                        {isLoading ? 'Loading...' : 'Fetch Workout Plans'}
+                    </button>
+                </form>
                 <ul>
-                    {workoutPlans.map(workoutPlan => (
+                    {Array.isArray(workoutPlans) && workoutPlans.map(workoutPlan => (
                         <li key={workoutPlan.Title}>{workoutPlan.Calories}</li>
                     ))}
                 </ul>
             </div>
         </>
+
     );
 };
 
-export default NutrationPlan;
+export default Nutration;
