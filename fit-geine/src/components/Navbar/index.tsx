d8e36dd2b469bb1210a8fa90eff3de836c4e9d53
mostafasayed2022 <116205995+mostafasayed2022@users.noreply.github.com>
2024-06-04 22:03:45 +0300

    dfg

--- fit-geine/src/components/Navbar/index.tsx
+++ fit-geine/src/components/Navbar/index.tsx
@@ -6,14 +6,13 @@ import "./navbar.scss";
 import logo from "../../images/logo.ico";
 import "./logoname.css";
 
-const Navbar = (props: { loggedIn: boolean; }) => {
+const Navbar = (props: { loggedIn: any; }) => {
   const navigate = useNavigate();
   const [menuOpen, setMenuOpen] = useState(false);
   const [size, setSize] = useState({
     width: 0,
     height: 0,
   });
-
   useEffect(() => {
     const handleResize = () => {
       setSize({
@@ -36,11 +35,6 @@ const Navbar = (props: { loggedIn: boolean; }) => {
     setMenuOpen((p) => !p);
   };
 
-  const handleLogout = () => {
-    localStorage.removeItem("token")
-    navigate("/login");
-  };
-
   return (
     <header className="header">
       <div className="header__content">
@@ -51,9 +45,11 @@ const Navbar = (props: { loggedIn: boolean; }) => {
           <p className="span1">Fit <span className="span2">Genie </span></p>
         </div>
 
+
         <nav
           className={`${"header__content__nav"} 
-          ${menuOpen && size.width < 768 ? `${"isMenu"}` : ""}`}
+          ${menuOpen && size.width < 768 ? `${"isMenu"}` : ""} 
+          }`}
         >
           <ul>
             <li>
@@ -66,35 +62,27 @@ const Navbar = (props: { loggedIn: boolean; }) => {
               <Link to="/pricing">Pricing</Link>
             </li>
             <li>
-              <Link to="/blog">Blog</Link>
+              <Link to="/Blog">Blog</Link>
             </li>
+
             <li>
               <Link to="/workout">Workout Plan</Link>
             </li>
+
             <li>
-              <Link to="/nutrition">Nutrition Plan</Link>
+              <Link to="/nutration">Nutrition Plan</Link>
             </li>
+
             <li>
-              <Link to="/contactus">Contact Us</Link>
+              <Link to="/ContactUs">ContactUs</Link>
             </li>
-            {props.loggedIn ? (
-              <li>
-                <button className="btn" onClick={handleLogout}>Logout</button>
-              </li>
-            ) : (
-              <>
-                <li>
-                  <Link to="/register">
-                    <button className="btn">Register</button>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/login">
-                    <button className="btn btn__login">Login</button>
-                  </Link>
-                </li>
-              </>
-            )}
+
+            <Link to="/register">
+              <button className="btn">Register</button>
+            </Link>
+            <Link to="/login">
+              <button className="btn btn__login">Login</button>
+            </Link>
           </ul>
         </nav>
         <div className="header__content__toggle">
