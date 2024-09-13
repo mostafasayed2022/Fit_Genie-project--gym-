e6cf3026dc38ff88779688f6041308f9b341d586
mostafasayed2022 <116205995+mostafasayed2022@users.noreply.github.com>
2024-09-14 02:40:21 +0300

    ff

--- fit-geine/src/components/Footer/index.tsx
+++ fit-geine/src/components/Footer/index.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import './footer.css';
 import logo from "../../images/logo.ico";
-
 import { Link } from 'react-router-dom';
 
-import contactBG from "../../images/contactBg.jpg";
-// import footerBG from "../../images/gymOverlayBg.jpg";
 
 
 
